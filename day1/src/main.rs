eaec1e1112f5200d40097bd1b1e57c381230ad95
Justin Lowen <justin.lowen@l7informatics.com>
2022-01-02 17:09:14 -0600

    Add error handling for file read step

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -10,11 +11,19 @@ fn main() {
         process::exit(1);
     });
 
-    println!("{:?}", args);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("In the file {}", &config.filename);
 
-    let contents = fs::read_to_string(&config.filename);
+    let contents = fs::read_to_string(&config.filename)?;
     println!("With text:\n {:?}", contents);
+    Ok(())
 }
 
 struct Config {
