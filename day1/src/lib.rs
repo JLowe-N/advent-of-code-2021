056ff9b505980871368485501bc840536d9e8dce
Justin Lowen <justin.lowen@l7informatics.com>
2022-01-02 17:32:51 -0600

    Set up line by line reading

--- day1/src/lib.rs
+++ day1/src/lib.rs
@@ -1,11 +1,22 @@
 use std::error::Error;
 use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("In the file {}", &config.filename);
 
-    let contents = fs::read_to_string(&config.filename)?;
-    println!("With text:\n {:?}", contents);
+    // let contents = fs::read_to_string(&config.filename)?;
+    let file = File::open(&config.filename).unwrap();
+    let reader = BufReader::new(file);
+
+    // Stream file line by line using lines() iterator
+    for (index, line) in reader.lines().enumerate() {
+        let line = line.unwrap(); // ignores errors
+                                  // do useful stuff
+        println!("{}. {}", index + 1, line);
+    }
+
     Ok(())
 }
 
