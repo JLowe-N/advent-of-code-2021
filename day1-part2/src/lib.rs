55844f490b82a828252c7f25c67ec6a27836c034
Justin Lowen <justin.lowen@l7informatics.com>
2022-01-03 13:45:14 -0600

    Implement day 1 - part 2 solution

--- day1-part2/src/lib.rs
+++ day1-part2/src/lib.rs
@@ -1,5 +1,4 @@
 use std::error::Error;
-use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -11,24 +10,31 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let reader = BufReader::new(file);
 
     // Stream file line by line using lines() iterator
-    let mut first = 0;
-    let mut second = 0;
+    let mut first: Vec<i32> = Vec::new();
+    let mut second: Vec<i32> = Vec::new();
     let mut num_increases: u32 = 0;
     for (index, line) in reader.lines().enumerate() {
         let line = line.unwrap(); // ignores errors
-        if index == 0 {
-            first = line.parse().unwrap();
-        } else if index == 1 {
-            second = line.parse().unwrap();
-            if second > first {
-                num_increases += 1
-            };
-        } else {
-            first = second;
-            second = line.parse().unwrap();
-            if second > first {
-                num_increases += 1
-            };
+        let number = line.parse().unwrap();
+
+        if first.len() < 3 {
+            first.push(number);
+        }
+        if index > 0 && second.len() < 3 {
+            second.push(number);
+        }
+        if first.len() == 3 && second.len() == 3 {
+            let first_sum: i32 = first.iter().sum();
+            let second_sum: i32 = second.iter().sum();
+            if second_sum > first_sum {
+                num_increases += 1;
+            }
+            first.rotate_left(1);
+            first.pop();
+            let pass_to_first = second[2];
+            second.rotate_left(1);
+            second.pop().unwrap();
+            first.push(pass_to_first);
         }
     }
 
